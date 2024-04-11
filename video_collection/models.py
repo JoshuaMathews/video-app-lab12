0123b67f7f53e0513dc378959ad516be3e7774db
mith <itzmrmith@gmail.com>
2024-04-10 20:13:25 -0500

    Video lab completion

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from urllib import parse
 
-# Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)
+
+    def save(self, *args, **kwargs):
+        try:
+            url_components = parse.urlparse(self.url) # feed it to parsing libary
+
+            # check if the url matches youtube's url formatting or deny it.
+            if url_components.scheme != "https" or url_components.netloc != "www.youtube.com" or url_components.path != '/watch':
+                raise ValidationError(f'Invalid Youtube URL {self.url}')
+
+            query_string = url_components.query
+
+            if not query_string: # No query string? Bad URL.
+                raise ValidationError(f'Invalid Youtube URL {self.url}')
+
+            parameters = parse.parse_qs(query_string, strict_parsing=True)
+            parameters_list = parameters.get('v')
+
+            if not parameters_list: # No parameters? No good youtube IDs.
+                raise ValidationError(f'Invalid Youtube parameters {self.url}')
+
+            self.video_id = parameters_list[0]
+
+        except ValueError as e:
+            raise ValidationError(f'Unable to parse URL {self.url}') from e
+
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+
+        if not self.notes:
+            self.notes = "No Notes."
+
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Video
+from .forms import VideoForm, SearchForm
+from django.contrib import messages
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from django.db.models.functions import Lower
+from pprint import pprint
 # Create your views here.
+
+def home(request):
+    app_name = 'Exercise Videos'
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+#You already added that video
+def add(request):
+    if request.method == "POST": # add a new video via post request.
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            try:
+                new_video_form.save()
+                return redirect('video_list') # save and redirect.
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL') # Bad URL.
+            except IntegrityError:
+                messages.warning(request, 'You already added that video') # Duplicate Video.
+
+        # form is not valid so we tell the user to check their data.
+        messages.warning(request, 'Check the data entered')
+        return render(request, 'video_collection/add.html', {'new_video_form' : new_video_form})
+
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form' : new_video_form})
+
+def video_list(request):
+    search_form = SearchForm(request.GET)
+
+    if search_form.is_valid(): # Make sure search is valid
+        search_term = search_form.cleaned_data['search_term']
+        videos = Video.objects.filter(name__icontains=search_term).order_by(Lower('name')) # order by search term
+    else:
+        search_form = SearchForm() # no search term so do by name exclusively.
+        videos = Video.objects.order_by(Lower('name'))
+
+    return render(request, 'video_collection/video_list.html', {'videos' : videos, 'search_form': search_form})
+
+def video_info(request, video_pk):
+    video = get_object_or_404(Video, pk=video_pk) # Use primary key for video indexing.
+    return render(request, 'video_collection/video_info.html', {'video': video})
